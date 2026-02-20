fab23c442bc7588428a0a10e21a919e4489f1fd8
miguelhard7-create <miguelhard7@gmail.com>
2026-02-20 18:50:31 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'mj-vip-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Ativação e limpeza de caches antigos
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estratégia de Cache: Tenta rede, se falhar, usa cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
